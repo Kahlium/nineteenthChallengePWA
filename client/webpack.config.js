2a7257c7b3aac21973c63766e74282c50c7bff6e
Kahlium <kahligtz@gmail.com>
2024-08-15 21:52:00 -0600

    finished the webpack file stuff

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -17,7 +17,32 @@ module.exports = () => {
     },
     plugins: [
       // TODO: Add and configure workbox plugins for a service worker and manifest file.
-      
+      new HtmlWebpackPlugin({
+        template: './index.html',
+        title: 'J.A.T.E'
+      }),      
+      new InjectManifest({
+        swSrc: './src-sw.js',
+        swDest: 'src-sw.js',
+      }),
+      new WebpackPwaManifest({
+        fingerprints: false,
+        inject: true,
+        name: 'Just Another Text Editor',
+        short_name: 'JATE',
+        description: 'Takes notes with JavaScript syntax highlighting!',
+        background_color: '#225ca3',
+        theme_color: '#225ca3',
+        start_url: '/',
+        publicPath: '/',
+        icons: [
+          {
+            src: path.resolve('src/images/logo.png'),
+            sizes: [96, 128, 192, 256, 384, 512],
+            destination: path.join('assets', 'icons'),
+          },
+        ],
+      }),
     ],
     
     // TODO: Add CSS loaders and babel to webpack.
